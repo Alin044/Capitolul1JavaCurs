4e2062c313e02322e06b1956ed993be6c7b748a2
Alin <alinalin03030@gmail.com>
2025-05-29 23:38:51 +0000

    Arrays exercise

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Arrays;
+
+public class Main {
+    public static void afis(int []v){
+        for(int i = 0; i < v.length; i++){
+            System.out.print(v[i] + " ");
         }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        int []a = {5, 2, 1, 7, 9, 3, 0};
+        afis(a);
+        Arrays.sort(a);
+        afis(a);
+
+        int poz = Arrays.binarySearch(a, 5);
+        System.out.println(poz >0 ? "Gasit pe pozitia " + poz : "Nu se gaseste !");
+        int []b = Arrays.copyOf(a, a.length);
+        afis(b);
     }
 }
