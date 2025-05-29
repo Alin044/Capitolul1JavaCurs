c6622a51c3efef7f2f8bd33036c4e27e5db23424
Alin <alinalin03030@gmail.com>
2025-05-29 23:42:02 +0000

    Arrays exercise

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,14 @@ public class Main {
         System.out.println(poz >0 ? "Gasit pe pozitia " + poz : "Nu se gaseste !");
         int []b = Arrays.copyOf(a, a.length);
         afis(b);
+
+        String[][] persoane = { {"Dl. ", "Dna. ", "Drsa. "}, {"Popescu", "Georgescu"}};
+
+        for(int i = 0; i < persoane.length; i++){
+            for(int j = 0; j < persoane[i].length; j++){
+                System.out.println(persoane[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
